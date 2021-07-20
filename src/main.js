3f449ee869e762ea63f819054ea2cf8e80f5f03d
unknown <442248832@qq.com>
2021-07-20 21:19:16 +0800

    main.js test

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 //import store from './store' //引入状态管理 store
-//vant样式
-import 'vant/lib/index.css'
-
+import 'vant/lib/index.css';
 // 这个库会在桌面端自动将mouse事件转换成对应的touch事件，使得组件能够在桌面端使用。
-import '@vant/touch-emulator'
-import { Toast } from 'vant'
-import 'lib-flexible'
-import ElementPlus from 'element-plus'
+import '@vant/touch-emulator';
+import { Toast } from 'vant';
+import 'lib-flexible';
+import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 
 createApp(App)
